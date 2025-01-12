fd002781611b55a41347ff33b49e394ae3e7a957
Mariana <marianaalmeidafga@gmail.com>
2025-01-12 11:50:37 -0300

    feat: finalizado questão de soma

--- iniciante/1001/1001.cpp
+++ iniciante/1001/1001.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int soma(int a, int b){
+    return a + b;
+}
+
 int main() {
-    cout << "Hello world!";
+    // Declaração
+    int a = 0; 
+    int b = 0;
+
+    // Atribuição
+    cin >> a; 
+    cin >> b;
+
+    // Soma
+    int x = soma(a, b);
+    cout << "X = " << x << endl;
+
     return 0;
 }
